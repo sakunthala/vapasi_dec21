6beac11ed25adbfd050b746c3ca0cc18a53c8fcd
Sakunthala <sakunthala@Sakunthalas-MacBook-Pro.local>
2021-11-29 08:48:51 +0530

    Remove the comment

--- chapters/9-callbacks/1-do-the order.js
+++ chapters/9-callbacks/1-do-the order.js
@@ -12,16 +12,3 @@ const f2 = () => {
     console.log('in f2');
 }
 
-f1(f2);
-
-
-
-
-// const f1 = (city, callback) => {
-//     console.log('fn', callback);
-//     return callback(city);
-// }
-
-// const print = (name) => console.log(`Name ${name}`);
-
-// f1('chennai', print);
